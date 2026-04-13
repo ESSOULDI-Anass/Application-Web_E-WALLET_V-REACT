ce5314d8bb1b90857294c363c805bac02d9ac24a
Anass Essouldi <essouldianass58@gmail.com>
2026-04-13 21:52:59 +0100

    first commit

---
+++ front-end/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import  Indexscreen from "./component/index";
+import Loging from './component/loging';
+import Dash from './component/dashboard';
+
+function App() {
+
+  return (
+    <>
+      <Dash/>
+    </>
+  )
+}
+
+export default App
---
+++ front-end/src/component/footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <>
+          
+            <p>&copy; 2026 E-Wallet. All rights reserved.</p>
+        </>
+    );
+}   
+export default Footer;
